5fd44dd662ea0d1a7a18709bf85098acf09d8080
Try Yoga Utomo <47817589+tyogautomo@users.noreply.github.com>
2019-03-26 00:02:31 +0700

    nambahin exercise-4.js

---
+++ exercise-4.js
@@ -0,0 +1,44 @@
+var tanggal = 1; //assign tanggal (1 sampai 31)
+var bulan = 8; //assign bulan (1 sampai 12)
+var tahun = 2030; //assign tahun (1900-2200)
+
+switch (bulan) {
+    case 1:
+        bulan = "Januari";
+        break;
+    case 2:
+        bulan = "Februari"
+        break;
+    case 3:
+        bulan = "Maret"
+        break;
+    case 4:
+        bulan = "April"
+        break;
+    case 5:
+        bulan = "Mei"
+        break;
+    case 6:
+        bulan = "Juni"
+        break;
+    case 7:
+        bulan = "Juli"
+        break;
+    case 8:
+        bulan = "Agustus"
+        break;
+    case 9:
+        bulan = "September"
+        break;
+    case 10:
+        bulan = "Oktober"
+        break;
+    case 11:
+        bulan = "November"
+        break;
+    case 12:
+        bulan = "Desember"
+        break;
+}
+
+console.log(tanggal + bulan + tahun);
