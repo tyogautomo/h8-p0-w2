c63b9d1421c7e7e159c717f23917d23899cffad0
Try Yoga Utomo <47817589+tyogautomo@users.noreply.github.com>
2019-03-28 23:38:56 +0700

    nambahin exercise-9

---
+++ exercise-9.js
@@ -0,0 +1,28 @@
+// Tugas 1 
+console.log("TUGAS 1");
+
+function shoutOut() {
+    return "Halo Function!!";
+}
+
+console.log(shoutOut());
+
+// Tugas 2
+console.log("-----------------------------------");
+console.log("TUGAS 2");
+
+function calculateMultiply(num1, num2) {
+    return num1 * num2
+}
+
+console.log(calculateMultiply(5, 6));
+
+// Tugas 3
+console.log("-----------------------------------");
+console.log("TUGAS 3")
+
+function processSentence(nama, umur, address, hobby) {
+    return "Nama saya " + nama + ", umur saya " + umur + " tahun, alamat saya di " + address + ", dan saya punya hobby yaitu " + hobby + "!"
+}
+
+console.log(processSentence("Yoga", "25", "Yogyakarta", "main game online & offline"));
